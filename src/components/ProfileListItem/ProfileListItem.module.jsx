71c5395be64525fb3e256855183c2b8f49a310ec
EvgenSalyakin <e.salyakin@gmail.com>
2024-02-09 01:58:25 +0100

    update

--- src/components/ProfileListItem/ProfileListItem.module.jsx
+++ src/components/ProfileListItem/ProfileListItem.module.jsx
@@ -3,9 +3,11 @@ import styles from "./ProfileListItem.module.scss";
 export const Name = ({ children }) => {
   return <h3 className={styles.name}>{children}</h3>;
 };
+
 export const ForbesItem = ({ children }) => {
   return <li className={styles.forbesItem}>{children}</li>;
 };
+
 export const Capital = ({ children }) => {
   return <span className={styles.capital}>{children}</span>;
 };
